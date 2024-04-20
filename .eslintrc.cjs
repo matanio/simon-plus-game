82d5f57d8c725d5de1d969d89c01180c8a4e56c7
Matan Yosef <matan.yosef02@gmail.com>
2024-04-21 11:20:33 +1200

    build: added tailwind linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,12 @@
 module.exports = {
     root: true,
-    env: { browser: true, es2020: true },
+    env: {browser: true, es2020: true},
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
         'plugin:prettier/recommended',
+        "plugin:tailwindcss/recommended"
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
@@ -13,7 +14,7 @@ module.exports = {
     rules: {
         'react-refresh/only-export-components': [
             'warn',
-            { allowConstantExport: true },
+            {allowConstantExport: true},
         ],
     },
 };
